69b15b8d182e0ee1c3c0b8c80275708b0b9e05fd
ANUJ PAL <anujpal2408@gmail.com>
2025-04-20 23:20:20 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @ 2025, CryptoDash - All Right Reserved.</p><br />
+      <p>-Developed by Anuj Pal</p>
+    </div>
+  )
+}
+
+export default Footer
