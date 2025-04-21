540a26ecd88ffd3fbb3692d1cd50a452560da6ef
ANUJ PAL <anujpal2408@gmail.com>
2025-04-21 18:40:11 +0530

    first commit

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -14,7 +14,7 @@ const CoinContextProvider = (props) => {
     const fetchAllCoin = async ()=> {
         const options = {
             method: 'GET',
-            headers: {accept: 'application/json', 'x-cg-demo-api-key': process.env.REACT_APP_API_KEY}
+            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-Hr24akFT7C4PSHQCsyRpcEhm'}
           };
           
           fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
